b83b8bf7811864bb39b0abc877da09bcb0637a91
Matthias Müller <matthias.mueller@trivadis.com>
2020-02-13 12:51:02 +0100

    WIP

--- Sources/Application/Areas/UnitTests/TestClasses/Services/Implementation/UnitTestFileInitializer.cs
+++ Sources/Application/Areas/UnitTests/TestClasses/Services/Implementation/UnitTestFileInitializer.cs
@@ -2,7 +2,6 @@
 using System.IO.Abstractions;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
